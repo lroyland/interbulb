f30b710346a7ad4dedccc374f92128f8a93080f4
Lucas Royland <muttering1@gmail.com>
2012-12-24 21:03:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interbulb::Application.config.secret_token = '425f37f95e4e0c15ab95b621305c9362267110fa153c64f328039c90b500bbdc3cde5bab5ebb01ddb9b1e4f62a723b09a42f503f1ff23fdd882960ecf90c3d8e'
